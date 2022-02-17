702891495be418c5602bd89649a1c050858ec144
NachoOlivares <99471653+NachoOlivares@users.noreply.github.com>
2022-02-17 19:11:05 +0100

    Add files via upload

---
+++ 07-microservice-seguros/src/main/java/com/whiteam/controller/CursosController.java
@@ -0,0 +1,71 @@
+package com.whiteam.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.security.RolesAllowed;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.whiteam.model.Curso;
+import com.whiteam.service.CursosService;
+
+@CrossOrigin("*")
+@RestController
+public class CursosController {
+
+	@Autowired
+	CursosService app;
+
+	@RolesAllowed({"ADMIN"})
+	@PostMapping(value = "curso", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void alta(@RequestBody Curso curso) {
+		app.AltaCurso(curso);
+	}
+
+	@PutMapping(value = "curso", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Curso actualizacion(@RequestBody Curso curso) {
+		return app.actualizarCurso(curso);
+	}
+
+	@RolesAllowed({"ADMIN", "OPERATOR"})
+	@GetMapping(value = "showall", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> ListCursos() {
+		return app.recuperarTodos();
+	}
+
+	@DeleteMapping(value = "curso/{denominacion}")
+	public boolean eliminar(@PathVariable("denominacion") String denominacion) {
+		if (app.recuperarTodos().stream().anyMatch(t -> t.getDenominacion().equals(denominacion))) {
+			app.eliminarCursos(denominacion);
+			return true;
+		}
+		return false;
+	}
+
+	//Ahora solo con rol ADMIN puedes acceder a este recurso
+	@RolesAllowed({"ADMIN"})
+	@GetMapping(value = "cursosduracion/{duracion}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> cursosCortos(@PathVariable("duracion") int duracion) {
+		return app.cursosDuracionMaxima(duracion);
+	}
+
+	@GetMapping(value = "cursostematica/{tematica}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> cursosTematica(@PathVariable("tematica") String tematica) {
+		return app.cusosPorTematica(tematica);
+	}
+
+	@GetMapping(value = "tematicas", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<String> tematicas() {
+		return app.recuperarTodos().stream().map(Curso::getTematica).distinct().collect(Collectors.toList());
+	}
+}
