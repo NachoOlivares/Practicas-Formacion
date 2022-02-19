6651cb889887a66ef253a038da7ac6e084fa7647
NachoOlivares <99471653+NachoOlivares@users.noreply.github.com>
2022-02-19 11:25:41 +0100

    Add files via upload

---
+++ 11_microservicio_crud_cursos_security_eureka/src/main/java/com/whiteam/service/CursosService.java
@@ -0,0 +1,14 @@
+package com.whiteam.service;
+
+import java.util.List;
+
+import com.whiteam.model.Curso;
+
+public interface CursosService {
+	void altaCurso(Curso curso);
+	Curso actualizarCurso(Curso curso);
+	List<Curso> recuperarTodos();
+	List<Curso> cursosDuracionMaxima(int duracion);
+	void eliminarCursos(String denominacion);
+	List<Curso> cursosPorTematica(String tematica);
+}
